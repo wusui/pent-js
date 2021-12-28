ded3c1347e7655f352778ad4db2d7c1b9349439f
Warren Usui <warrenusui@earthlink.net>
2021-12-27 16:15:33 -0800

    Make pentomino.js functional

---
+++ nonfunctionalCaller.js
@@ -0,0 +1,10 @@
+/**
+ * (c) 2021 Warren Usui MOPFPPP
+ * This code is licensed under the MIT license (see LICENSE.txt for details)
+ */
+
+/**
+ * Non-functional call to the pentomino solver.
+ */
+console.log(require('./pentomino').pentominoRectangles());
+
--- pentomino.js
+++ pentomino.js
@@ -5,7 +5,7 @@
 
 /**
  * Main routine
- * 
+ *
  * Find all solutions for filling rectangles with pentominos.
  */
 const tTreeMain = require('./tree_main_branch');
@@ -17,31 +17,78 @@ const rectFindPent = require('./rect_find_pent');
 const rectPlacePent = require('./rect_place_pent');
 const pentUtils = require('./pent_utils');
 
-//tree = tTreeMain.treeData();
-//xxx = rectGetX.solverGetRectsWithX();
-
-const pentominoRectangles = () =>
-  pentUtils.rangeNum(2, 0).
+/**
+ * Solve all rectangular pentomino tilings.
+ *
+ * First loop through all possible retangle dimensions
+ *
+ * @return {String} pentomino solutions
+ */
+exports.pentominoRectangles = () =>
+  pentUtils.rangeNum(1, 0).
   map(x => solveSize(x)).reduce((a, b) => a + b, '');
 
+/**
+ * Get solutions for each size.
+ *
+ * @param {Integer} x Index for each possible rectangle size
+ * @return {String} Solutions for a given pentomino size
+ */
 const solveSize = x => handlePlacements(x,
   tTreeMain.treeData(), rectGetX.solverGetRectsWithX());
 
-const handlePlacements = (x, tree, rect) =>
-  rect[x].map(x => solver(tree, x)).reduce((a,b) => a + b, '');
+/**
+ * Loop through possible X-pentomino locations for this rectangle
+ *
+ * @param {Integer} size Index of rectangle size
+ * @param {Array} tree tree
+ * @param {Array} rect Board layout
+ * @return {String} Solutions for a given pentomino size
+ */
+const handlePlacements = (size, tree, rect) =>
+  rect[size].map(x => solver(tree, x)).reduce((a,b) => a + b, '');
 
-const solver = (tree, rect) => {
-  if (! rectHoles.areHolesValidSizes(rect)) return '';
-  if (rectSymmetry.checkSym(rect)) return '';
-  if (rect.flat(1).find(x => x == '.') == undefined) {
-    return pentOutput.pentToString(rect);
-  }
-  pents = rectFindPent.getPlacements(tree)(rect);
-  if (pents === []) return '';
-  return pents.reduce((a, b) => a + (solver(tree,
-          rectPlacePent.putPentInRect(tree, rect, b))), '');
-}
+/**
+ * Find all solutions for a given rectangle configuration
+ *
+ * This routine is essentially the heart of this program.  It
+ * (in coordination with getNextPent) recursively tries pentominos,
+ * backtracking if failures are found.
+ *
+ * It first makes sure that there are no holes in the rectangle of a size
+ * that is not evenly divisible by 5.  Such holes can not be filled with
+ * an integer number of pentominos.
+ *
+ * Next it makes sure there are no W-flipping symmetry issues,
+ * and then sees if there are any open squares are left in rect.
+ *
+ * If there are no open squares, then we have a solution which is returned.
+ * Otherwise, we find all possible open squares and pass that value to
+ * getPlacements
+ *
+ * @param {Array} tree tree
+ * @param {Array} rect Board layout
+ * @return {Array} List of solutions for the pentomino solver
+ */
+const solver = (tree, rect) =>
+  ! rectHoles.areHolesValidSizes(rect) ? '' :
+  rectSymmetry.checkSym(rect) ? '' :
+  rect.flat(1).find(x => x == '.') == undefined ?
+  pentOutput.pentToString(rect) :
+  getNextPent(tree, rect, rectFindPent.getPlacements(tree)(rect));
 
-console.log(pentominoRectangles());
-// console.log(pentUtils.rangeNum(4,0));
-// console.log(xxx[0].map(x => solver(tree, x)).reduce((a,b) => a + b, ''));
+/**
+ * If there are no possible placements, return blank and return
+ * (essentially backtracking up the recursive calls adding pentominos).
+ *
+ * Otherwise, place the solution into the rectangle and recursively call
+ * solver.
+ *
+ * @param {Array} tree tree
+ * @param {Array} rect Board layout
+ * @param {Array} pents List of node numbers in tree that fit
+ */
+const getNextPent = (tree, rect, pents) =>
+  pents == 0 ? '' :
+  pents.reduce((a, b) => a + (solver(tree,rectPlacePent.
+  putPentInRect(tree, rect, b))), '');
